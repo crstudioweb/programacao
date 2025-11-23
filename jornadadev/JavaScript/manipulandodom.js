e0970b429225614bd020525742955172f6e39c5b
crstudioweb <cr.studioweb@gmail.com>
2025-11-23 03:30:36 -0300

    aula de JavaScript

---
+++ jornadadev/JavaScript/manipulandodom.js
@@ -0,0 +1,16 @@
+//capturando um elemento
+//let titulo1 = document.getElementById("titulo1");
+
+//capturando por classe
+
+//let titulo2 = document.getElementsByClassName("titulo2");
+
+//capturando por tags
+
+//let tags = document.getElementsByTagName()
+
+//console.log(titulo1);
+//console.log(titulo2);
+
+let novotitulo = (document.getElementById("nome").innerHTML =
+  prompt("Qual seu nome?"));
